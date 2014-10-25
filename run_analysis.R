c80582752802568f186df5ce9202c8fde7c456da
Tiago Otto <tiagootto@hotmail.com>
2014-10-25 18:43:12 +0100

    alter script

--- run_analysis.R
+++ run_analysis.R
@@ -1,19 +1,30 @@
 
+###########################
+# Read all data
+###########################
 
-# Step1
 trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
 trainLabel <- read.table("./UCI HAR Dataset/train/y_train.txt")
 trainSubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
 testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
 testLabel <- read.table("./UCI HAR Dataset/test/y_test.txt")
 testSubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
+
+
+###########################
+# Step1
+###########################
+
 joinData <- rbind(trainData, testData)
 joinLabel <- rbind(trainLabel, testLabel)
 joinSubject <- rbind(trainSubject, testSubject)
 
+###########################
 # Step2
+###########################
 
-features <- read.table("./UCI HAR Dataset/features.txt")
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
 joinData <- joinData[, meanStdIndices]
 names(joinData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) 
@@ -21,9 +32,10 @@ names(joinData) <- gsub("mean", "Mean", names(joinData))
 names(joinData) <- gsub("std", "Std", names(joinData)) 
 names(joinData) <- gsub("-", "", names(joinData)) 
 
+###########################
 # Step3
+###########################
 
-activity <- read.table("./UCI HAR Dataset/activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
 substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
@@ -31,29 +43,32 @@ activityLabel <- activity[joinLabel[, 1], 2]
 joinLabel[, 1] <- activityLabel
 names(joinLabel) <- "activity"
 
+###########################
 # Step4
+###########################
 
 names(joinSubject) <- "subject"
 cleanedData <- cbind(joinSubject, joinLabel, joinData)
 #write.table(cleanedData, "merged_data.txt") # write the 1st dataset
 
+###########################
 # Step5
+###########################
 
-subjectLen <- length(table(joinSubject)) # 30
-activityLen <- dim(activity)[1] # 6
-columnLen <- dim(cleanedData)[2]
-result <- matrix(NA, nrow=subjectLen*activityLen, ncol=columnLen)
-result <- as.data.frame(result)
-colnames(result) <- colnames(cleanedData)
+finalData <- as.data.frame(matrix(NA,
+                                  nrow=nrow(activity)*length(table(joinSubject)),
+                                  ncol=ncol(cleanedData))
+                           )
+colnames(finalData) <- colnames(cleanedData)
 row <- 1
-for(i in 1:subjectLen) {
-    for(j in 1:activityLen) {
-        result[row, 1] <- sort(unique(joinSubject)[, 1])[i]
-        result[row, 2] <- activity[j, 2]
+for( i in 1:length(table(joinSubject)) ) {
+    for(j in 1:nrow(activity)) {
+        finalData[row, 1] <- sort(unique(joinSubject)[, 1])[i]
+        finalData[row, 2] <- activity[j, 2]
         bool1 <- i == cleanedData$subject
         bool2 <- activity[j, 2] == cleanedData$activity
-        result[row, 3:columnLen] <- colMeans(cleanedData[bool1&bool2, 3:columnLen])
+        finalData[row, 3:ncol(cleanedData) ] <- colMeans(cleanedData[bool1&bool2, 3:ncol(cleanedData)])
         row <- row + 1
     }
 }
-write.table(result, "tidy_data.txt")
+write.table(finalData, "tidy_data.txt")
